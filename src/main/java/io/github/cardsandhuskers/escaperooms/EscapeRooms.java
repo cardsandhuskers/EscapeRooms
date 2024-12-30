80bc823a58284d74a71dfe89fec31c14ac2eb521
Mitch <cardsandhuskers@gmail.com>
2024-12-30 15:29:33 -0600

    plugin shell

---
+++ src/main/java/io/github/cardsandhuskers/escaperooms/EscapeRooms.java
@@ -0,0 +1,17 @@
+package io.github.cardsandhuskers.escaperooms;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EscapeRooms extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
